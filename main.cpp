94995a1c9e577a28358bf058d1058870209f3756
Riley E Wilbur <riley.wilbur@uvm.edu>
2018-05-07 18:11:47 -0400

    Added datatypes folder and test type,  reformatted the  initialize function

--- main.cpp
+++ main.cpp
@@ -1,56 +1,87 @@
 #include <iostream>
-#include <openssl/conf.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
 #include <lcm/lcm-cpp.hpp>
 #include <boost/program_options.hpp>
+#include "datatypes/test_t.hpp"
+#include <csignal>
 
-
-
-using namespace std;
 namespace po = boost::program_options;
+
 
 
 lcm::LCM *m_lcm;
 std::string lcm_url = "udpm://239.255.76.67:7667?ttl=1";
+datatypes::test_t test;
 
 
-void initialize(){
-    m_lcm = new lcm::LCM(lcm_url);
-    if(!m_lcm->good()){
-        cerr << "LCM Failed to initialize" << std::endl;
-    }
+void sigHandler(int signum);
+void initialize(int ac, const char *av[]) __attribute__ ((cold));
 
-}
+int main(int ac, const char *av[]);
+
+void send_test();
 
 int main(int ac, const char *av[])
 {
+    initialize(ac, av);
+    signal(SIGINT, sigHandler);
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while(1){
+        test.timestamp = 0;
+        m_lcm->publish("TEST", &test);
+
+        usleep(5);
+
+
+
+    }
+#pragma clang diagnostic pop
+
+
+
+
+
+}
+
+
+void sigHandler(int signum){
+    std::cout << "Exiting...\n";
+
+    delete m_lcm;
+
+    exit(signum);
+}
+
+void initialize( int ac, const char *av[] ){
     po::options_description desc("Allowed options");
     desc.add_options()
-        ("help", "produce help message")
-        ("lcm_url", po::value<string>(), "set LCM URL")
-    ;
+            ("help", "produce help message")
+            ("lcm_url", po::value<std::string>(), "set LCM URL")
+            ;
 
     po::variables_map vm;
     po::store(po::parse_command_line(ac, av, desc), vm);
     po::notify(vm);
 
     if (vm.count("help")) {
-        cout << desc << "\n";
-        return 1;
+        std::cout << desc << "\n";
+        exit(1);
     }
     if (vm.count("lcm_url")) {
-        cout << "LCM URL was set to "
-     << vm["lcm_url"].as<string>() << ".\n";
-        lcm_url = vm["lcm_url"].as<string>();
+        std::cout << "LCM URL was set to "
+                  << vm["lcm_url"].as<std::string>() << ".\n";
+        lcm_url = vm["lcm_url"].as<std::string>();
     }
     else {
-        cout << "Using default LCM URL";
+        std::cout << "Using default LCM URL";
     }
 
- initialize();
-
-
+    m_lcm = new lcm::LCM(lcm_url);
+    if(!m_lcm->good()){
+        std::cerr << "LCM Failed to initialize" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
 
 
